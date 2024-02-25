49e8e0f3f90a35a98cb225b199e56c7760a09dbc
relan1997 <harshal.relan.201116@gmail.com>
2024-02-25 11:29:38 +0530

    Completed Todo-List

---
+++ todo-list/src/contexts/TodoContext.js
@@ -0,0 +1,21 @@
+import {createContext,useContext} from 'react'
+
+export const TodoContext = createContext({
+    todos:[
+        {
+            id:1,
+            todo:"Todo Msg",
+            completed:false
+        }
+    ],
+    addTodo: (todo)=>{}, // hum function ki definition yaha define nahi karte hai woh usually define tumhare App.jsx mai hoti hai
+    updateTodo:(todo,id)=>{},
+    deleteTodo:(id)=>{},
+    toggleComplete:(id)=>{}
+})
+
+export const TodoProvider=TodoContext.Provider;
+
+export default useTodo=()=>{
+    return useContext(TodoContext);
+}
